035a81c0f60027dcdaffd1bf42b8110b837e7d4d
Malinowsk <juanrago2015@gmail.com>
2022-05-06 21:47:07 -0300

    Agrego avances en el codigo menu

--- TrabajoDeCursada/JuanMenu/menu.c
+++ TrabajoDeCursada/JuanMenu/menu.c
@@ -3,6 +3,7 @@
 #include <unistd.h>  //  para usar el sleep
 
 void menu();
+int menu_nivel_2();
 
 int main(){
 
@@ -31,19 +32,72 @@ void menu(){
         printf("\n");
 
         switch (opcion) {
-            case 1:
-                printf("Opcion 1\n");
+            case 1:  // pi
+
+                int opcion_1;
+                do
+                {
+                    opcion_1 = menu_nivel_2();
+                switch (opcion_1)
+                {
+                case 1:
+                    int numStep1;
+                    printf("\n");
+                    printf("Ingresa numero de step:\n");
+                    printf("\n");
+                    scanf("%d", &numStep1);
+                    //ejecuto algoritmo de pi con el parametro numstep
+                    break;
                 
+                case 2:
+                    int numStep2;
+                    printf("\n");
+                    printf("Ingresa numero de step:\n");
+                    printf("\n");
+                    scanf("%d", &numStep2);
+                    break;
+                     //ejecuto algoritmo de pi con el parametro numstep // caso 1 y 2 se juntan
+                }
+                } while (opcion_1 != 3);
+            
                 break;
                 
             case 2:
-                printf("Opcion 2\n");
                 
+                int opcion_3;
+                do
+                {
+                    opcion_3 = menu_nivel_2();
+                switch (opcion_3)
+                {
+                case 1:
+                    int n1;
+                    printf("\n");
+                    printf("Ingresa el tamaño de la matriz:\n");
+                    printf("\n");
+                    scanf("%d", &n1);
+                    //ejecuto algoritmo de pi con el parametro numstep
+                    break;
+                
+                case 2:
+                    int n2;
+                    printf("\n");
+                    printf("Ingresa el tamaño de la matriz:\n");
+                    printf("\n");
+                    scanf("%d", &n2);
+                    break;
+                     //ejecuto algoritmo de pi con el parametro numstep // caso 1 y 2 se juntan
+                }
+                } while (opcion_1 != 3);
+            
                 break;
                 
             case 3:
                 printf("Opcion 3\n");
-                            
+                    printf("\n");  
+                    printf("Aca se describe el harware de la compu:\n");
+                    printf("\n");  
+                    sleep(1);
                 break;
 
             case 4:
@@ -58,4 +112,21 @@ void menu(){
     }
     while (opcion!=4);    // ejecuta el do hasta que la variable opcion no sea 4
     
+}
+
+int menu_nivel_2(){
+    int opcion;
+    system("clear"); // limpia la pantalla
+
+    printf("\n");
+    printf("Que tipo de paralelismo desea usar:\n");
+    printf("\n");
+    printf("    1- sin reduccion\n");
+    printf("    2- con reduccion\n");
+    printf("    3- atras\n");
+    printf("\n");
+    printf("Ingrese una opción:  ");
+    scanf("%d", &opcion); // pide al usuario que ingrese lo que desea hacer
+    printf("\n");
+    return opcion;
 }
